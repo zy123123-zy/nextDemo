ccbb72da822bf4a8d359c845f4fdfce5e0a3c6e6
zy123123-zy <712956400TS@protonmail.com>
2024-11-18 11:11:48 +0800

    feat: todo init;

--- app/todoList/page.tsx
+++ app/todoList/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <p>todo</p>
-} 
+import AddTask from '@/ui/todoList/AddTask.tsx';
+import TaskList from '@/ui/todoList/TaskList.tsx';
+import { TasksProvider } from '@/ui/todoList/TasksContext.tsx';
+
+export default function TaskApp() {
+  return (
+    <TasksProvider>
+      <h1>Day off in Kyoto</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
+  );
+}
